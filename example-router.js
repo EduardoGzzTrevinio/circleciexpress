60fb201d952766d1e4bd59fc99a581789005d3a3
fhernandez4791 <71672579+fhernandez4791@users.noreply.github.com>
2021-05-06 09:32:56 -0500

    testFJHR06052021t2

--- example-router.js
+++ example-router.js
@@ -1,4 +1,4 @@
 function exampleRouteHandler(req, res) {
-  res.send("Goodbye EarthlingFJHR!");
+  res.send("Goodbye EarthlingFJHR2!");
 }
 module.exports = exampleRouteHandler;
--- test.js
+++ test.js
@@ -10,7 +10,7 @@ describe("Example Test 1", () => {
     const mockResponse = httpMocks.createResponse();
     exampleRouteHandler(mockRequest, mockResponse);
     const actualResponseBody = mockResponse._getData();
-    const expectedResponseBody = "Goodbye Earthling!FJHR";
+    const expectedResponseBody = "Goodbye EarthlingFJHR2!";
     assert(actualResponseBody, expectedResponseBody);
   });
 });
